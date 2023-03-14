6be092f09de44e00fcb92ee159fd20764ba529f3
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2023-03-14 11:50:13 -0300

    useTodo added

--- useTodo/useTodo.js
+++ useTodo/useTodo.js
@@ -1,5 +1,5 @@
 import { useEffect, useReducer } from "react";
-import { todoReducer } from "../08-Reducer/todoReducer";
+import { todoReducer } from "./todoReducer";
 
 const initialState = [];
 const init = () => {
